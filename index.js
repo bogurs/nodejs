976e81f6617bb08186ea950d94d8d5075bab1d78
bogurs <thsong10@gmail.com>
2018-06-27 21:38:04 +0900

    사용자생성 성공/실패에 대한 supertest

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 var users = [
     {id: 1, name: 'alice'},
     {id: 2, name: 'bek'},
@@ -8,6 +9,8 @@ var users = [
 ];
 
 app.use(morgan('dev'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/users', function (req, res) {
   req.query.limit = req.query.limit || 10;
@@ -33,6 +36,18 @@ app.delete('/users/:id', (req, res) => {
 
   users = users.filter(user=> user.id !== id);
   res.status(204).end();
+});
+
+app.post('/users', (req, res) => {
+  const name = req.body.name;
+  if(!name) return res.status(400).end();
+  const isConflict = users.filter(user => user.name === name).length;
+  if(isConflict) return res.status(409).end();
+
+  const id = Date.now();
+  const user = {id, name};
+  users.push(user);
+  res.status(201).json(user);
 });
 
 app.listen(3000, function () {
