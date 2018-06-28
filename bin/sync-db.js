8f5b71e13f555235498834d9f67794ddc6588b4a
bogurs <thsong10@gmail.com>
2018-06-28 17:40:27 +0900

    테스트인 경우 DB를 전체 삭제, 테스트가 아닌 경우 유지하고 동기화

--- bin/sync-db.js
+++ bin/sync-db.js
@@ -1,5 +1,8 @@
 const models = require('../models');
 
 module.exports = () => {
-    return models.sequelize.sync({force: true}); //force: true는 기존에 DB가 있더라도 재생성하겠다는 의미이다.
+    const options = {
+        force: process.env.NODE_ENV === 'test' ? true : false
+    };
+    return models.sequelize.sync(options); //force: true는 기존에 DB가 있더라도 재생성하겠다는 의미이다.
 }
