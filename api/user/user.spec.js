a5fc7e7f2f562e6232a915c7b06be6383efb2df6
bogurs <thsong10@gmail.com>
2018-06-28 16:58:43 +0900

    데이터베이스와 destroy컨트롤러 연동

--- api/user/user.spec.js
+++ api/user/user.spec.js
@@ -36,7 +36,7 @@ describe('GET /users는', ()=>{
     });
 });
 
-describe.only('GET /users/1은', ()=>{
+describe('GET /users/1은', ()=>{
     describe('성공시', ()=>{
         it('id가 1인 유저 객체를 반환한다 ', (done)=> {
             request(app)
@@ -64,7 +64,7 @@ describe.only('GET /users/1은', ()=>{
     });
 });
 
-describe('DELETE /users/1', ()=>{
+describe.only('DELETE /users/1', ()=>{
     describe('성공시', ()=>{
         it('204를 응답한다 ', (done)=> {
             request(app)
