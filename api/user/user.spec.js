1f5505df31ec2e499c6c664d745287672566f233
bogurs <thsong10@gmail.com>
2018-06-28 16:55:14 +0900

    데이터베이스와 show컨트롤러 연동(모카 자동 테스트 재수행 기능 추가)

--- api/user/user.spec.js
+++ api/user/user.spec.js
@@ -3,7 +3,7 @@ const should = require('should');
 const app = require('../../');
 const models = require('../../models');
 
-describe.only('GET /users는', ()=>{
+describe('GET /users는', ()=>{
     describe('성공시', ()=>{
         const users = [{name: 'alice'}, {name: 'bek'}, {name: 'chris'}];
         before(()=>models.sequelize.sync({force: true}));
@@ -36,7 +36,7 @@ describe.only('GET /users는', ()=>{
     });
 });
 
-describe('GET /users/1은', ()=>{
+describe.only('GET /users/1은', ()=>{
     describe('성공시', ()=>{
         it('id가 1인 유저 객체를 반환한다 ', (done)=> {
             request(app)
